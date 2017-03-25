82dc2b28cc38df742fd0ca514f6398407c9882ae
Katie <katiearina@gmail.com>
2017-03-24 20:40:26 -0400

    :smile_cat: Game is working in most ways. Just need to add panels for win/loss condition for each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,127 +2,123 @@
 // VARIABLE DECLARATIONS!
 var startButton;
 var i;
+var wins = 0;
+var losses = 0;
 var right;
-var number = 6;
+var number;
 
 var walrusQuestions =[{
     question: "What is scientific name for the walrus?",
     choices: ["Astichopus multifidus", "Mirounga angustirostris", "Monodon monoceros", "Odobenus rosmarus"],
-    correctAnswer: 3
+    correctAnswer: "Odobenus rosmarus"
 }, {
  	question: "Which of these is not a common predator of the walrus?",
     choices: ["Elephant Seal", "Human", "Polar Bear", "Killer Whale"],
-    correctAnswer: 0
+    correctAnswer: "Elephant Seal"
 }, {
  	question: "Only male walruses grow tusks",
     choices: ["True", "False"],
-    correctAnswer: 1
+    correctAnswer: "False"
 }, {
  	question: "A walrus can remain underwater how long before coming up for air?",
     choices: ["30 minutes", "1 hour", "3 hours", "6 hours"],
-    correctAnswer: 0
+    correctAnswer: "30 minutes"
 }, {
  	question: "What is untrue of walrus tusks?",
     choices: ["They are used to create breathing holes in ice floes", "Tusk size helps determine social status", "They stop growing when the walrus reaches adulthood", "They are used to pull the walrus out of the water"],
-    correctAnswer: 2
+    correctAnswer: "They stop growing when the walrus reaches adulthood"
 }, {
  	question: "How long is the gestation period for a walrus?",
     choices: ["6 months", "9 months", "13 months", "15 months"],
-    correctAnswer: 3
+    correctAnswer: "15 months"
 }, {
  	question: "Walruses are which of the following?",
-    choices: ["Herbivorous", "Omnivorous", "Carnivorous", "Pescetarians"],
-    correctAnswer: 2
+    choices: ["Herbivores", "Omnivores", "Carnivores", "Pescetarians"],
+    correctAnswer: "Carnivores"
 }, {
  	question: "The Latin name for walrus (Odobenus Rosmarus) means which of the following?",
     choices: ["Whisker-bearing ocean-beast", "Tooth-walking sea-horse", "XXX", "XXX"],
-    correctAnswer: 2
+    correctAnswer: "Tooth-walking sea-horse"
 }];
 
 //---------------------------------------------------------------------------
 // FUNCTION DECLARATIONS!
 
-// setTimeout(gameLoad, 1000);
 function run() {
-	intervalId = setInterval(decrement, 1000);
-}
-
-function decrement() {
-	number--;
-    $("#countdown").html("<h2>" + number + "</h2>");
-		if (number === 0) {
-		stop();
-		setTimeout(nextQuestion, 1000);
-		}
+	countdownTimer = setInterval(decrement, 1000);
 }
 
 function stop() {
-	clearInterval(intervalId);
+	clearInterval(countdownTimer);
 }
 
-// This function starts game over completely/creates start button
+// This function creates start button
 function createStartButton() {
-	startButton = $("<button>");
-	startButton.addClass("btn btn-info");
-	startButton.text("Start the Game!");
-	$("#main-content").append(startButton);
+	$("#quiz-content").html("<button class='start-button'>Start the Game!</button>");
 }
 
+// This function runs on page load
 function gameLoad() {
-	$(".quiz-div").hide();
 	createStartButton();
-	// console.log("I've loaded gameLoad!");
-	// console.log(walrusQuestions[0].question);
-	// console.log(walrusQuestions[0].choices[1]);
-	// console.log(walrusQuestions[0].correctAnswer);
-	// var right = walrusQuestions[0].correctAnswer;
-	// console.log(walrusQuestions[0].choices[right]);
 }
+
 
 function gameStop() {
-	$("#main-heading").html("<h1>Walrus Trivia Game!</h1>");
-	$(".quiz-div").hide();
-	createStartButton();
 	stop();
+	$("#quiz-content").html("<h2>You answered " + wins + " questions correctly!</h2>"
+		+ "<h2>You answered " + losses + " questions incorrectly!</h2>")
+}
+
+// This writes the countdown timer to the page
+function decrement() {
+    $("#countdown-timer").html("<h4>You have " + number + " seconds remaining</h4>");
+    number--;
+		if (number < 0) {
+		$("#countdown-timer").html("<h4>Time's up!</h4>");
+		stop();
+		setTimeout(nextQuestion, 1000);
+		losses++;
+		}
 }
 
 // This function actually starts the game on button press!
 function gameStart() {
 	console.log("You clicked the button!");
-	run();
-	$(".btn-info").hide();
-	$(".quiz-div").show();
 	i = 0;
-	$("#question-div").html("<h3>" + walrusQuestions[i].question + "</h3");
-	$("#answer-1").html("<h4>" + walrusQuestions[i].choices[0] + "</h4");
-	$("#answer-2").html("<h4>" + walrusQuestions[i].choices[1] + "</h4");
-	$("#answer-3").html("<h4>" + walrusQuestions[i].choices[2] + "</h4");
-	$("#answer-4").html("<h4>" + walrusQuestions[i].choices[3] + "</h4");
-	console.log("I've loaded gameStart!");
-	console.log(walrusQuestions[i].question);
-	console.log(walrusQuestions[i].correctAnswer);
-	right = walrusQuestions[i].correctAnswer;
-	console.log(walrusQuestions[i].choices[right]);
+	$("#countdown-timer").html("<h4>You have " + 30 + " seconds remaining</h4>");
+	number = 29;
+	run();
+	$("#quiz-content").empty();
+	$("#quiz-content").append("<h3>" + walrusQuestions[i].question + "</h3");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[0] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[1] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[2] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[3] + "</h4");
 }
 
 function nextQuestion() {
 	i = i + 1;
-	if (i < walrusQuestions.length) {
-	number = 6;
+	$("#countdown-timer").html("<h4>You have " + 30 + " seconds remaining</h4>");
+	number = 29;
 	run();
-	// console.log(walrusQuestions[i].question);
-	$("#question-div").html("<h3>" + walrusQuestions[i].question + "</h3");
-	$("#answer-1").html("<h4>" + walrusQuestions[i].choices[0] + "</h4");
-	$("#answer-2").html("<h4>" + walrusQuestions[i].choices[1] + "</h4");
-	$("#answer-3").html("<h4>" + walrusQuestions[i].choices[2] + "</h4");
-	$("#answer-4").html("<h4>" + walrusQuestions[i].choices[3] + "</h4");
-	console.log("I've loaded nextQuestion!");
-	console.log(walrusQuestions[i].question);
-	console.log(walrusQuestions[i].correctAnswer);
-	right = walrusQuestions[i].correctAnswer;
-	console.log(walrusQuestions[i].choices[right]);
+	if (i === 2) {
+	$("#quiz-content").empty();
+	$("#quiz-content").append("<h3>" + walrusQuestions[i].question + "</h3");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[0] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[1] + "</h4");
+	// $("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[2] + "</h4");
+	// $("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[3] + "</h4");
+}
+	else if (i < walrusQuestions.length && i != 2) {
+	$("#quiz-content").empty();
+	$("#quiz-content").append("<h3>" + walrusQuestions[i].question + "</h3");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[0] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[1] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[2] + "</h4");
+	$("#quiz-content").append("<h4 class='answers'>" + walrusQuestions[i].choices[3] + "</h4");
 }
 	else {
+		$("#countdown-timer").empty();
 		gameStop();
 	}
 }
@@ -138,10 +134,28 @@ function alertConsole() {
 gameLoad();
 
 // When "Start Game" button is clicked, start the game!
-$(".btn-info").click(function() {
-  gameStart();
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();
+	gameStart();
 });
 
-$(document).on("click", ".answers-div", alertConsole);
-
-
+// 
+$("body").on("click", ".answers", function(event){
+	console.log($(this).text());
+	if ($(this).text() === walrusQuestions[i].correctAnswer) {
+		// alert("You did it!");
+		wins++;
+		console.log("Wins: " + wins);
+		stop();
+		setTimeout(nextQuestion, 0);
+		// nextQuestion();
+	}
+	else {
+		// alert("Womp womp");
+		losses++;
+		console.log("Losses: " + losses);
+		stop();
+		setTimeout(nextQuestion, 0);
+		// nextQuestion();
+	}
+});
